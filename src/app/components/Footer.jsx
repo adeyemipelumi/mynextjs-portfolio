f558e17a8dd53fd973e4ddcd75719be6dfe8755a
ZYLLAH <adetunjipelumi610@gmail.com>
2023-11-17 09:46:24 +0100

    First commit

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+import navLogo from "public/navLogo.png"
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>
+          <Image src={navLogo} height={30} alt="" />
+        </span>
+        <p className="text-slate-600">All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
